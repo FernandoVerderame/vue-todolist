2625af2ec30221c7a3a9da5e8aa84db7bb02bfb5
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-23 16:19:30 +0100

    added array list item into data.js

---
+++ js/data.js
@@ -0,0 +1,7 @@
+/* Preparo l'array di oggetti della todo list*/
+const tasks = [
+    { id: 1, done: false, text: 'Fare la spesa' },
+    { id: 2, done: true, text: 'Lavare i capelli' },
+    { id: 3, done: false, text: 'Comprare una marca da bollo' },
+    { id: 4, done: false, text: 'Aggiornare il PC' }
+  ];
