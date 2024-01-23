e29f906ba1a3ac9e46af32e964193026551df0c8
FernandoVerderame <fernandoverderame96@hotmail.com>
2024-01-23 17:22:05 +0100

    added delete task function on click button

--- js/script.js
+++ js/script.js
@@ -4,7 +4,14 @@ const app = createApp({
     name: 'VueTodoList',
     data: () => ({
         tasks
-    })
+        
+    }),
+
+    methods: {
+        deleteTask(id) {
+            this.tasks = this.tasks.filter(task => id !== task.id);
+        }
+    }
 });
 
 app.mount('#root');
